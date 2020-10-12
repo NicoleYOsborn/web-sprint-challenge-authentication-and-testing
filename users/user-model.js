6b5c1dbecb475af960ca8f8fc56e902699d28314
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-10-11 18:52:19 -0700

    authentication middleware completed

--- users/user-model.js
+++ users/user-model.js
@@ -15,7 +15,7 @@ function findBy(filter){
     return db('users').where(filter);
 }
 
-function add(user){
+async function add(user){
     const [id] = await db('users').insert(user);
     return findById(id)
 }
